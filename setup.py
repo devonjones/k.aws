e00b2e6dc824fdf401f1bdd412ed7b95c7b5181a
“Devon <devon.jones@sendgrid.com>
2014-05-08 17:11:29 -0600

    fixing aws-env

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ def parse_dependency_links(file_name):
 
 setup(
 	name = "kaws",
-	version = "1.0.4",
+	version = "1.0.5",
 	url = "https://wiki.knewton.net/index.php/Tech",
 	author = "Devon Jones",
 	author_email = "devon.jones@gmail.com",
